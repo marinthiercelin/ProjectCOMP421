37bd692af3d6bfed6d2931e4836484510d38f1a2
ehoelzl <edoardo.holzl@epfl.ch>
2017-02-18 16:49:38 -0500

    drop

---
+++ drop_everything.sql
@@ -0,0 +1,19 @@
+drop table manager;
+drop table forrent;
+drop table salesman;
+drop table paysfor;
+drop table rates;
+drop table rents;
+drop table buys;
+drop table fee;
+drop table forsale;
+drop table forrent;
+drop table employee;
+drop table product;
+drop table client;
+drop table Branch;
+drop table payment;
+
+drop type producttype;
+drop type pymtmethod;
+
