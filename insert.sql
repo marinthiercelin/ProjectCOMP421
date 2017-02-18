fcf09a616b796ffd0260af48d20496c34b5549a7
marinthiercelin <marin.thiercelin@epfl.ch>
2017-02-18 17:04:12 -0500

    insertions

--- insert.sql
+++ insert.sql
@@ -1,2 +1,7 @@
-/*Insertions*/
-INSERT INTO Client VALUES (1, "Joseph D'Silva", )
+﻿/*Insertions*/
+INSERT INTO Client VALUES (2, 'Tarek Holzl', 420, 'St-Catherine St. West','Montreal QC','Canada', CURRENT_DATE);
+INSERT INTO Client VALUES (3, 'Marin Thiercelin', 8, 'Maisonneuve Bd. East','Montreal QC','Canada', CURRENT_DATE);
+INSERT INTO Client VALUES (4, 'William Burgess', 789, 'Parc St.','Montreal QC','Canada', CURRENT_DATE);
+INSERT INTO Client VALUES (5, 'Clara Kang', 56, 'Rachel St. East','Montreal QC','Canada', CURRENT_DATE);
+INSERT INTO Client VALUES (7, 'Luke Skywalker', 7, 'Main Street','Mos Esley','Tatooine', '04/05/0900');
+
