a9cecbe0f316d9c9cc4d0dc80170db631282c220
ehoelzl <edoardo.holzl@epfl.ch>
2017-02-18 16:58:54 -0500

    drop

--- drop_everything.sql
+++ drop_everything.sql
@@ -1,19 +1,22 @@
-drop table manager;
-drop table forrent;
-drop table salesman;
-drop table paysfor;
-drop table rates;
-drop table rents;
-drop table buys;
-drop table fee;
-drop table forsale;
-drop table forrent;
-drop table employee;
-drop table product;
-drop table client;
-drop table Branch;
-drop table payment;
+drop table product CASCADE;
+drop table employee CASCADE;
+drop table branch CASCADE;
+drop table manager CASCADE;
+drop table salesman CASCADE;
+drop table client CASCADE;
+drop table payment CASCADE;
+drop table rents CASCADE;
+drop table buys CASCADE;
+drop table forrent CASCADE;
+drop table forsale CASCADE;
+drop table fee CASCADE;
+drop table paysfor CASCADE;
+drop table rates CASCADE;
 
 drop type producttype;
 drop type pymtmethod;
+
+
+
+
 
