6a0f31324d06dc78027fea4856dee2d9d54e7120
marinthiercelin <marin.thiercelin@epfl.ch>
2017-02-21 17:40:43 -0500

    finished updates

--- modification.sql
+++ modification.sql
@@ -2,7 +2,7 @@
 were created more than 7years ago 
 and that haven't made any payement in the last 2 years , (check that they have no currently going renting) delete all payments, and ratings associated with them*/
 
-SELECT *
+/*SELECT *
 FROM client 
 WHERE CURRENT_DATE - 6*365 >  creationDate  AND cid NOT IN (
 
@@ -15,7 +15,7 @@ WHERE c.cid = p.cid AND CURRENT_DATE - 2*365  < p.pyDate
 SELECT c.cid
 FROM client c , rents r 
 WHERE c.cid = r.cid AND r.endDate >= CURRENT_DATE
-);
+);*/
 
 DELETE 
 FROM client 
@@ -35,11 +35,11 @@ WHERE c.cid = r.cid AND r.endDate >= CURRENT_DATE
 
 /* 2 give a 10% raise to all the salesman that have a rating average greater than 4.5/5 */
 
-SELECT s.eid, AVG(rating)
+/*SELECT s.eid, AVG(rating)
 FROM Salesman s, Rates r
 WHERE s.eid = r.eid
 GROUP BY (s.eid)
-HAVING AVG(rating) >= 0;
+HAVING AVG(rating) >= 0;*/
 
 UPDATE Employee
 SET salary = 1.1*salary
@@ -59,9 +59,9 @@ WHERE eid IN (
 	check that no fees have 0 products, delete the ones who do
 */
 
-SELECT *
+/*SELECT *
 FROM Product p, ForRent f
-WHERE p.PrId = f.PrId AND p.pType = 'Snowboots' AND p.Available = true AND 2017-p.pYear > 3;
+WHERE p.PrId = f.PrId AND p.pType = 'Snowboots' AND p.Available = true AND 2017-p.pYear > 3;*/
 
 DELETE FROM Product p
 USING Forrent f
@@ -73,6 +73,7 @@ WHERE p.PrId = f.PrId AND p.pType = 'Snowboots' AND p.Available = true AND EXTRA
 merge 2 branches (bid1, bid2) in bi1 delete the manager of branch bid2 and the branch bid2  
 */
 
+/*
 SELECT *
 FROM Employee
 WHERE Bid = 2;
@@ -80,6 +81,16 @@ WHERE Bid = 2;
 SELECT *
 FROM Product
 WHERE Bid = 2;
+
+SELECT *
+FROM Manager
+WHERE Bid = 2;
+
+SELECT *
+FROM Branch
+WHERE Bid = 2;
+*/
+
 
 UPDATE Product
 SET Bid = 1 
@@ -89,17 +100,9 @@ UPDATE Employee
 SET Bid = 1
 WHERE Bid = 2;
 
-SELECT *
-FROM Manager
-WHERE Bid = 2;
-
 DELETE FROM Employee e
 USING Manager man
 WHERE e.eid = man.eid and man.Bid = 2;
-
-SELECT *
-FROM Branch
-WHERE Bid = 2;
 
 DELETE FROM Branch 
 WHERE Bid = 2;
