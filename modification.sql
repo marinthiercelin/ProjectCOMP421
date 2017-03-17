1a7881276de4ab99334478d7739a316c49037dd7
marinthiercelin <marin.thiercelin@epfl.ch>
2017-03-16 23:10:11 -0400

    modif of assignment2

--- modification.sql
+++ modification.sql
@@ -35,11 +35,16 @@ WHERE c.cid = r.cid AND r.endDate >= CURRENT_DATE
 
 /* 2 give a 10% raise to all the salesman that have a rating average greater than 4.5/5 */
 
-SELECT s.eid, AVG(rating)
-FROM Salesman s, Rates r
-WHERE s.eid = r.eid
-GROUP BY (s.eid)
-HAVING AVG(rating) >= 4.5;
+/*SELECT eid, ename, salary FROM Employee 
+WHERE eid IN (
+	SELECT s.eid
+	FROM Employee e, Salesman s, Rates r
+	WHERE s.eid = r.eid
+	GROUP BY (s.eid)
+	HAVING AVG(rating) >= 4.5
+)
+ORDER BY salary;
+*/
 
 UPDATE Employee
 SET salary = 1.1*salary
@@ -59,9 +64,11 @@ WHERE eid IN (
 	check that no fees have 0 products, delete the ones who do
 */
 
-/*SELECT *
+/*
+SELECT *
 FROM Product p, ForRent f
-WHERE p.PrId = f.PrId AND p.pType = 'Snowboots' AND p.Available = true AND 2017-p.pYear > 3;*/
+WHERE p.PrId = f.PrId AND p.pType = 'Snowboots' AND p.Available = true AND 2017-p.pYear > 3;
+*/ 
 
 DELETE FROM Product p
 USING Forrent f
@@ -100,7 +107,7 @@ SET Bid = 1
 WHERE  Bid = 2;
 
 UPDATE Employee
-SET Bid = 1
+SET Bid = 1 
 WHERE Bid = 2;
 
 DELETE FROM Employee e
@@ -110,22 +117,6 @@ WHERE e.eid = man.eid and man.Bid = 2;
 DELETE FROM Branch 
 WHERE Bid = 2;
 
-
-/* 5
-look for the cheapest skis (and possibly most recent) available in a certain branch and make a client buy them starting now
-and make them pay for it to a salesman 
-*/
-/*
-CREATE VIEW temp1 AS
-SELECT s.PrId PrId, s.Price Price, p.pYear pYear
-FROM ForSale s, Product p
-WHERE p.Bid = 1 AND s.PrId = p.PrId AND p.pType = 'Ski' AND p.Available = true 
-ORDER BY Price ASC, pYear DESC
-LIMIT 1;
-
-WITH temp1
-INSERT INTO Payment VALUES(12, 0, 'cash', temp1.Price, 2, 4); /* PyId dscnt, mthd, amnt, eid, cid*/
-*/
 
 
 
